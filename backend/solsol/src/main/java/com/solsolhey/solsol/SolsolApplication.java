ada2b6e0a8323e0886fa5fb1a684599e36ceae6c
canit0221 <rladydtn78@gmail.com>
2025-08-21 08:37:27 +0900

    refactor: Spring Boot 설정 최적화

--- backend/solsol/src/main/java/com/solsolhey/solsol/SolsolApplication.java
+++ backend/solsol/src/main/java/com/solsolhey/solsol/SolsolApplication.java
@@ -9,6 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SolsolApplication {
 
 	public static void main(String[] args) {
+		// .env 파일 경로 설정 (상위 디렉토리의 .env 파일 사용)
+		System.setProperty("dotenv.path", "../.env");
+		
 		SpringApplication.run(SolsolApplication.class, args);
 	}
 
