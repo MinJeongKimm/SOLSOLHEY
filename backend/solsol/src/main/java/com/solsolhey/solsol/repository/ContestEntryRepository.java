cb883752dd761e0e0edb3a9c541a35c19b28a701
MinJeongKimm <min99093072@gmail.com>
2025-08-21 21:03:17 +0900

    Fix:  userRepository 초기화 오류 수정

--- backend/solsol/src/main/java/com/solsolhey/solsol/repository/ContestEntryRepository.java
+++ backend/solsol/src/main/java/com/solsolhey/solsol/repository/ContestEntryRepository.java
@@ -1,6 +1,7 @@
 package com.solsolhey.solsol.repository;
 
-import com.solsolhey.solsol.entity.ContestEntry;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.solsolhey.solsol.entity.ContestEntry;
 
 @Repository
 public interface ContestEntryRepository extends JpaRepository<ContestEntry, Long> {
